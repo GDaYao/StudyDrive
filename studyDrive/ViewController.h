838cd4cd6db1a83f8d827a44508dce26a029d615
GDaYao <1418154979@qq.com>
2017-05-16 11:14:41 +0800

    Initial Commit

---
+++ studyDrive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  studyDrive
+//
+//  Created by GDaYao on 2017/5/16.
+//  Copyright © 2017年 GDaYao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ studyDrive/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  studyDrive
+//
+//  Created by GDaYao on 2017/5/16.
+//  Copyright © 2017年 GDaYao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
